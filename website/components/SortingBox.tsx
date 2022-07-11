27148626a28b3a569fb4ddad65044f53a34d7e50
Henrik Liisberg <henrik.giil.liisberg@nav.no>
2022-07-11 15:44:57 +0200

    Added next.js styling

--- website/components/SortingBox.tsx
+++ website/components/SortingBox.tsx
@@ -1,27 +1,30 @@
 import { Checkbox, CheckboxGroup, Search, Select } from "@navikt/ds-react";
-import { useState } from "react";
+
+import Layout from "../components/layout";
 
 export default function SortingBox() {
   const handleChange = (val: any[]) => {};
   return (
     <div>
-      <Search label="Sorter resultatene" size="medium" variant="simple" />
+      <Layout>
+        <Search label="Sorter resultatene" size="medium" variant="simple" />
 
-      <CheckboxGroup
-        legend="Hvor vil du sitte?"
-        onChange={(val: any[]) => handleChange(val)}
-        size="medium"
-      >
-        <Checkbox value="Under utbedring">Under utbedring</Checkbox>
-        <Checkbox value="Planlagt">Planlagt</Checkbox>
-        <Checkbox value="Ferdig">Ferdig</Checkbox>
-      </CheckboxGroup>
+        <CheckboxGroup
+          legend="Velg status:"
+          onChange={(val: any[]) => handleChange(val)}
+          size="medium"
+        >
+          <Checkbox value="Under utbedring">Under utbedring</Checkbox>
+          <Checkbox value="Planlagt">Planlagt</Checkbox>
+          <Checkbox value="Ferdig">Ferdig</Checkbox>
+        </CheckboxGroup>
 
-      <Select label="Hvilket land har du bosted i." size="medium">
-        <option value="">Velg kategori</option>
-        <option value="komponent">Komponent</option>
-        <option value="figma">Figma</option>
-      </Select>
+        <Select label="Velg kategori:" size="medium">
+          <option value="">Velg kategori</option>
+          <option value="komponent">Komponent</option>
+          <option value="figma">Figma</option>
+        </Select>
+      </Layout>
     </div>
   );
 }
