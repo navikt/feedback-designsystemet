5b556e01ee22af710fe7b2f077e80e096e4ff8e6
Per Holte <per.havard.skogli.holte@nav.no>
2022-07-14 12:25:39 +0200

    Updated header

--- website/components/Heading.tsx
+++ website/components/Heading.tsx
@@ -1,12 +1,38 @@
 import "@navikt/ds-css";
 import "@navikt/ds-css-internal";
-import {Header} from "@navikt/ds-react-internal";
+import { Braille, LightBulb } from "@navikt/ds-icons";
+import { ToggleGroup } from "@navikt/ds-react";
+import { Header } from "@navikt/ds-react-internal";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Heading() {
-    return (
-        <Header>
-            <Header.Title as="h1">Sykepenger</Header.Title>
-             <Header.User name="Ola Normann" />
-        </Header>
-    )
+  const [value, setValue] = useState("forslag");
+  const router = useRouter();
+
+  const handleChange = (x) => {
+    setValue(x);
+    router.push("/" + x);
+  };
+
+  return (
+    <Header>
+      <Header.Title as="h1">Feedback</Header.Title>
+      <ToggleGroup
+        className="toggleGroup"
+        onChange={handleChange}
+        value={value}
+        size="medium"
+      >
+        <ToggleGroup.Item className="forslag" value="forslag">
+          <LightBulb aria-hidden />
+          Forslag
+        </ToggleGroup.Item>
+        <ToggleGroup.Item className="roadmap" value="roadmap">
+          <Braille aria-hidden />
+          Roadmap
+        </ToggleGroup.Item>
+      </ToggleGroup>
+    </Header>
+  );
 }
--- website/components/Navigation.tsx
+++ website/components/Navigation.tsx
@@ -6,23 +6,5 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 
 export default function Navigation() {
-  const [value, setValue] = useState("forslag");
-  const router = useRouter();
-
-  const handleChange = (x) => {
-    setValue(x);
-    router.push("/" + x);
-  };
-  return (
-    <ToggleGroup onChange={handleChange} value={value} size="medium">
-      <ToggleGroup.Item value="forslag">
-        <LightBulb aria-hidden />
-        Forslag
-      </ToggleGroup.Item>
-      <ToggleGroup.Item value="roadmap">
-        <Braille aria-hidden />
-        Roadmap
-      </ToggleGroup.Item>
-    </ToggleGroup>
-  );
+  return <></>;
 }
