4678400cf985785c92fe1e80234a93aa1a7c0dfd
Henrik Liisberg <henrik.giil.liisberg@nav.no>
2022-07-19 16:58:28 +0200

    Changes to voting-panel colors

--- website/components/VotingPanel.tsx
+++ website/components/VotingPanel.tsx
@@ -1,3 +1,4 @@
+import { Tooltip } from "@navikt/ds-react";
 import { useState } from "react";
 
 const VotingPanel = () => {
@@ -10,69 +11,77 @@ const VotingPanel = () => {
         {"< Tilbake"}
       </a>
 
-      <p className="mx-10 my-auto">
-        Echo Three to Echo Seven, Han, old buddy, do you read me?
-      </p>
+      <p className="mx-10 my-auto">Hvor viktig er denne endringen for deg?</p>
       <ul className="grid grid-cols-3 gap-x-5 m-10 max-w-md mx-auto">
-        <li className="relative">
-          <input
-            className="sr-only peer"
-            type="radio"
-            value="yes"
-            name="answer"
-            id="answer_yes"
-          ></input>
-          <label
-            className="flex p-5 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-green-500 peer-checked:ring-2 peer-checked:border-transparent"
-            htmlFor="answer_yes"
-          >
-            Nah
-          </label>
+        <Tooltip content="Dette ville vært nyttig" placement="bottom">
+          <li className="relative">
+            <input
+              className="sr-only peer"
+              type="radio"
+              value="yes"
+              name="answer"
+              id="answer_yes"
+            ></input>
+            <label
+              className="place-content-center flex p-5 bg-white border border-border rounded-lg cursor-pointer focus:outline-none 
+            hover:bg-interaction-primary-hover-subtle 
+            peer-checked:ring-green-500 peer-checked:bg-green-200 peer-checked:ring-2 peer-checked:border-transparent"
+              htmlFor="answer_yes"
+            >
+              Litt
+            </label>
 
-          <div className="absolute hidden w-5 h-5 peer-checked:block top-5 right-3">
-            👍
-          </div>
-        </li>
+            {/* <div className="absolute hidden w-5 h-5 peer-checked:block top-5 right-3">
+            🙂
+          </div> */}
+          </li>
+        </Tooltip>
+        <Tooltip content="Dette er viktig for meg" placement="bottom">
+          <li className="relative">
+            <input
+              className="sr-only peer"
+              type="radio"
+              value="no"
+              name="answer"
+              id="answer_no"
+            ></input>
+            <label
+              className="place-content-center flex p-5 bg-white border border-border rounded-lg cursor-pointer 
+            focus:outline-none hover:bg-interaction-primary-hover-subtle 
+            peer-checked:ring-orange-500 peer-checked:bg-orange-200 peer-checked:ring-2 peer-checked:border-transparent"
+              htmlFor="answer_no"
+            >
+              Viktig
+            </label>
 
-        <li className="relative">
-          <input
-            className="sr-only peer"
-            type="radio"
-            value="no"
-            name="answer"
-            id="answer_no"
-          ></input>
-          <label
-            className="flex p-5 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-red-500 peer-checked:ring-2 peer-checked:border-transparent"
-            htmlFor="answer_no"
-          >
-            Si
-          </label>
-
-          <div className="absolute hidden w-5 h-5 peer-checked:block top-5 right-3">
+            {/* <div className="absolute hidden w-5 h-5 peer-checked:block top-5 right-3">
             👎
-          </div>
-        </li>
+          </div> */}
+          </li>
+        </Tooltip>
+        <Tooltip content="Dette er kritisk for meg" placement="bottom">
+          <li className="relative">
+            <input
+              className="sr-only peer"
+              type="radio"
+              value="maybe"
+              name="answer"
+              id="answer_maybe"
+            ></input>
+            <label
+              className="place-content-center flex p-5 bg-white border border-border rounded-lg cursor-pointer 
+            focus:outline-none hover:bg-interaction-primary-hover-subtle 
+            peer-checked:ring-red-500 peer-checked:bg-red-200 peer-checked:ring-2 peer-checked:border-transparent"
+              htmlFor="answer_maybe"
+            >
+              Kritisk
+            </label>
 
-        <li className="relative">
-          <input
-            className="sr-only peer"
-            type="radio"
-            value="maybe"
-            name="answer"
-            id="answer_maybe"
-          ></input>
-          <label
-            className="flex p-5 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-yellow-500 peer-checked:ring-2 peer-checked:border-transparent"
-            htmlFor="answer_maybe"
-          >
-            Siuuu
-          </label>
-
-          <div className="absolute hidden w-5 h-5 peer-checked:block top-5 right-3">
+            {/* <div className="absolute hidden w-5 h-5 peer-checked:block top-5 right-3">
             🤔
-          </div>
-        </li>
+          </div> */}
+          </li>
+        </Tooltip>
       </ul>
     </div>
   );
