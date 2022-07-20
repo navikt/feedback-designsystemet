feee203451029eec8d89d36b3cbe833c8422d49c
Henrik Liisberg <henrik.giil.liisberg@nav.no>
2022-07-20 14:42:58 +0200

    Removed gray background in app.js

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -2,10 +2,9 @@ import "../styles/globals.css";
 import "../components/header/Heading.css";
 import Heading from "../components/header/Heading";
 
-
 export default function MyApp({ Component, pageProps }) {
   return (
-    <div className="bg-gray-200">
+    <div>
       <Heading />
       <Component {...pageProps} />
     </div>
