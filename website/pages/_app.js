5ccc9bf23016c9ee58a71520e3987a7b66dde079
Per Holte <per.havard.skogli.holte@nav.no>
2022-07-20 15:04:50 +0200

    #1 - updated yaml

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,11 +1,18 @@
 import "../styles/globals.css";
 import "../components/header/Heading.css";
 import Heading from "../components/header/Heading";
-
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <div className="bg-gray-200">
+      <Head>
+        <title>Feedbacksystemet</title>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+        ></meta>
+      </Head>
       <Heading />
       <Component {...pageProps} />
     </div>
