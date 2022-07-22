36188e2e5422e5fce4d48ffd8a357bc3a5fb99e7
Henrik Liisberg <henrik.giil.liisberg@nav.no>
2022-07-22 14:26:09 +0200

    Centered Cards in mobile view

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -2,10 +2,9 @@ import "../styles/globals.css";
 import "../components/header/Heading.css";
 import Heading from "../components/header/Heading";
 
-
 export default function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <div className="flex flex-col mx-auto justify-center">
       <Heading />
       <Component {...pageProps} />
     </div>
