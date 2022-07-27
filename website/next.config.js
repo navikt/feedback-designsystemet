2f45aa8813a062e888d6577ef4e5f2094209666f
Per Holte <per.havard.skogli.holte@nav.no>
2022-07-27 10:48:45 +0200

    #1 - next config changes

--- website/next.config.js
+++ website/next.config.js
@@ -1,17 +1,26 @@
+const withTM = require("next-transpile-modules")(["@navikt/ds-tokens"]);
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 // @ts-check
 
 /**
  * @type {import('next').NextConfig}
  **/
-const config = {
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-    azureAppClientId: process.env.AZURE_APP_CLIENT_ID,
-    azureJwksUri: process.env.AZURE_OPENID_CONFIG_JWKS_URI,
-    azureAppIssuer: process.env.AZURE_OPENID_CONFIG_ISSUER,
-    azureAppWellKnownUrl: process.env.AZURE_APP_WELL_KNOWN_URL,
-    azureAppJWK: process.env.AZURE_APP_JWK,
-  },
+const config = () => {
+  withBundleAnalyzer(
+    withTM({
+      serverRuntimeConfig: {
+        // Will only be available on the server side
+        azureAppClientId: process.env.AZURE_APP_CLIENT_ID,
+        azureJwksUri: process.env.AZURE_OPENID_CONFIG_JWKS_URI,
+        azureAppIssuer: process.env.AZURE_OPENID_CONFIG_ISSUER,
+        azureAppWellKnownUrl: process.env.AZURE_APP_WELL_KNOWN_URL,
+        azureAppJWK: process.env.AZURE_APP_JWK,
+      },
+    })
+  );
 };
 
 const STUDIO_REWRITE = {
@@ -22,7 +31,5 @@ const STUDIO_REWRITE = {
       : "/studio/index.html",
 };
 
-module.exports = {
-  config,
-  rewrites: () => [STUDIO_REWRITE],
-};
+module.exports = config();
+rewrites: () => [STUDIO_REWRITE];
