f2b40abeafe7d3d13563f189833c5f9d27135883
Per Holte <per.havard.skogli.holte@nav.no>
2022-07-28 13:11:20 +0200

    #1 - still trying to fetch

--- website/components/header/Heading.tsx
+++ website/components/header/Heading.tsx
@@ -4,16 +4,16 @@ import { Braille, LightBulb } from "@navikt/ds-icons";
 import { ToggleGroup } from "@navikt/ds-react";
 import { Header } from "@navikt/ds-react-internal";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import HeaderLogo from "../../public/HeaderLogo";
 
-export interface IData {
+export interface IPerson {
   status: number;
   name: string;
   mail?: string;
   ident?: string;
 }
-const Heading: React.FC<IData> = (user) => {
+const Heading: React.FC<IPerson> = (user) => {
   const [value, setValue] = useState("forslag");
   const router = useRouter();
 
@@ -51,9 +51,18 @@ const Heading: React.FC<IData> = (user) => {
 
 export async function getServerSideProps() {
   // Fetch data from external API
-  const user = await fetch("/api/auth").then((res) => {
-    console.log(res);
-    res.json();
+  const user = await fetch(`/api/auth`).then(async (res) => {
+    const json = await res.json();
+    if (json?.status === 200) {
+      return json;
+    } else {
+      return {
+        status: 200,
+        name: "Feil Feilesen",
+        mail: "feil@nav.no",
+        ident: "H12456",
+      };
+    }
   });
 
   return { props: { user } };
