fb71a4d1239ad442954ff733edb6edaa6719c883
Henrik Liisberg <henrik.giil.liisberg@nav.no>
2022-08-01 13:23:18 +0200

    Put Cards in unnumbered lists

--- website/components/CardList.tsx
+++ website/components/CardList.tsx
@@ -10,34 +10,40 @@ export interface CardProps {
 const CardList: React.FC<CardProps> = ({ posts, category, filters }) => {
   if (category)
     return (
-      <div className="grid justify-center mx-auto mt-10 gap-x-12 gap-y-4 md:grid-cols-2 lg:grid-cols-3">
+      <ul className="grid justify-center mx-auto mt-10 gap-x-12 gap-y-4 md:grid-cols-2 lg:grid-cols-3">
         {posts
           .filter((post) => post.state == category)
           .map((post, index) => (
-            <Card key={category + " " + index} card={post} />
+            <li>
+              <Card key={category + " " + index} card={post} />
+            </li>
           ))}
-      </div>
+      </ul>
     );
 
   if (filters && filters.length > 0)
     return (
-      <div className="grid justify-center mx-auto mt-10 gap-x-12 gap-y-4 md:grid-cols-2 lg:grid-cols-3 ">
+      <ul className="grid justify-center mx-auto mt-10 gap-x-12 gap-y-4 md:grid-cols-2 lg:grid-cols-3 ">
         {posts
           .filter((post) =>
             post.tags ? filters.some((f) => post.tags.includes(f)) : false
           )
           .map((post, index) => (
-            <Card key={category + " " + index} card={post} />
+            <li>
+              <Card key={category + " " + index} card={post} />
+            </li>
           ))}
-      </div>
+      </ul>
     );
 
   return (
-    <div className="grid justify-center mx-auto mt-10 gap-x-12 gap-y-4 md:grid-cols-2 lg:grid-cols-3">
+    <ul className="grid justify-center mx-auto mt-10 gap-x-12 gap-y-4 md:grid-cols-2 lg:grid-cols-3">
       {posts.map((post, index) => (
-        <Card key={category + " " + index} card={post} />
+        <li>
+          <Card key={category + " " + index} card={post} />
+        </li>
       ))}
-    </div>
+    </ul>
   );
 };
 
