a87f4acf52724fcc765b20e0a9be48481d86e8d5
Henrik Liisberg <henrik.giil.liisberg@nav.no>
2022-08-02 13:28:05 +0200

    Removed filtering

--- website/components/CardList.tsx
+++ website/components/CardList.tsx
@@ -4,7 +4,6 @@ import Card, { ICard } from "../components/Card";
 export interface CardProps {
   posts: ICard[];
   category?: String;
-  filters?: string[];
 }
 
 const CardList: React.FC<CardProps> = ({ posts, category, filters }) => {
@@ -17,21 +16,6 @@ const CardList: React.FC<CardProps> = ({ posts, category, filters }) => {
           .filter((post) => post.state == category)
           .map((post, index) => (
             <li className="mx-auto" key={index}>
-              <Card key={category + " " + index} card={post} />
-            </li>
-          ))}
-      </ul>
-    );
-
-  if (filters && filters.length > 0)
-    return (
-      <ul className="grid list-none justify-center mx-auto mt-10 gap-x-12 gap-y-4 md:grid-cols-2 lg:grid-cols-3 ">
-        {posts
-          .filter((post) =>
-            post.tags ? filters.some((f) => post.tags.includes(f)) : false
-          )
-          .map((post, index) => (
-            <li key={index}>
               <Card key={category + " " + index} card={post} />
             </li>
           ))}
--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,14 +1,7 @@
 import client from "../lib/sanity/sanity";
 import { ICard } from "../components/Card";
 import CardList from "../components/CardList";
-import {
-  Accordion,
-  BodyShort,
-  Checkbox,
-  CheckboxGroup,
-  Select,
-} from "@navikt/ds-react";
-import { useState } from "react";
+import { BodyShort } from "@navikt/ds-react";
 import Divider from "../components/Divider";
 
 export interface PostProps {
@@ -21,40 +14,13 @@ export interface Tag {
 }
 
 const Home: React.FC<PostProps> = ({ posts, tags }) => {
-  const [filters, setFilters] = useState<string[]>(null);
-
   return (
     <>
-      {/* <Accordion>
-        <Accordion.Item>
-          <Accordion.Header>Filtrering</Accordion.Header>
-          <Accordion.Content>
-            <CheckboxGroup
-              legend="Velg status:"
-              onChange={(v) => setFilters(v)}
-              size="small"
-            >
-              {tags &&
-                tags.map((tag, index) => (
-                  <Checkbox key={index} value={tag.title}>
-                    {tag.title}
-                  </Checkbox>
-                ))}
-            </CheckboxGroup>
-
-            <Select label="Velg kategori:" size="medium">
-              <option value="">Velg kategori</option>
-              <option value="komponent">Komponent</option>
-              <option value="figma">Figma</option>
-            </Select>
-          </Accordion.Content>
-        </Accordion.Item>
-      </Accordion> */}
       <Divider name="Stem!" />
       <BodyShort className="text-center text-gray-800">
         Her kan du stemme på oppgaver Team Designsystemet skal jobbe med
       </BodyShort>
-      <CardList posts={posts} filters={filters} />
+      <CardList posts={posts} category="Open" />
     </>
   );
 };
@@ -70,7 +36,8 @@ export async function getStaticProps() {
       _updatedAt,
       slug,
       votes,
-      "tags":tags[]->title
+      "tags":tags[]->title,
+      "state":state->title
     }
     `
   );
