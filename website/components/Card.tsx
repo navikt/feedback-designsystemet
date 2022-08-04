a2fc79a3ba2ca3a279575258bb008e7e8f07289b
Per Holte <per.havard.skogli.holte@nav.no>
2022-08-04 15:58:46 +0200

    css - changes

--- website/components/Card.tsx
+++ website/components/Card.tsx
@@ -31,7 +31,7 @@ const Card: React.FC<CardProps> = ({ card }) => {
       <li
         aria-label={title}
         tabIndex={0}
-        className="flex flex-col text-text p-2 md:h-60 h-40 max-w-md min-w-[28rem] ease-in-out duration-300 
+        className="flex flex-col text-text p-2 h-60  max-w-md min-w-full md:min-w-[28rem] ease-in-out duration-300 
         hover:scale-[1.02] hover:bg-interaction-primary-hover-subtle 
         shadow-xl border border-gray-400 rounded-lg cursor-pointer mx-auto m-2 bg-canvas-background-light"
       >
--- website/components/MainOverview.tsx
+++ website/components/MainOverview.tsx
@@ -7,7 +7,7 @@ import Timeline from "./Timeline";
 const MainOverview: React.FC<PostProps> = ({ posts }) => {
   return (
     <div className="max-w-[2200px] mx-auto">
-      <p className="mt-20 mx-[20rem] text-left">
+      <p className="mt-20 mx-[5rem] lg:mx-[20rem] text-center">
         {
           "Did you hear that? They've shut down the main reactor. We'll be destroyed for sure. This is madness! We're doomed! There'll be no escape for the Princess this time. What's that? Artoo! Artoo-Detoo, where are you? At last! Where have you been? They're heading in this direction. And where you come from. I saw part of the message he was. I seem to have found it. General Kenobi, years ago you served my father in the Clone."
         }
